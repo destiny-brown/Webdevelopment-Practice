7f20c9b02a86a9f35607dc41cc2aca17dbddcda1
Destiny Brown <119943684+destiny-brown@users.noreply.github.com>
2025-11-30 09:09:55 +0000

    part A done

--- [HKU] Assignment Four/restapi.js
+++ [HKU] Assignment Four/restapi.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
     /* Implement the logic here */
-
+//Connect to Mongodb
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://mongodb/HKO')
 .then(() => {
@@ -16,6 +12,26 @@ mongoose.connect('mongodb://mongodb/HKO')
  console.log("MongoDB connection error: "+err);
 })
 
+//Connection Events, from w3schools
+const db = mongoose.connection;
+
+// Connection successful
+db.once('open', () => console.log('Connected to MongoDB'));
+
+// Connection error
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // terminate app
+});
+
+// Connection disconnected
+db.on('disconnected', () => {
+    console.error('Disconnected from MongoDB. Terminating the app.');
+    process.exit(1); // terminate app
+});
+
+
+//set the schema
 var Schema = mongoose.Schema;
 
 var weatherDatabase = new Schema({
@@ -27,15 +43,8 @@ var weatherDatabase = new Schema({
     Rainfall: Number
 })
 
-
-
-
-
-
-
-
-
-
+//Create my model
+var myWeather = mongoose.model("dailydata", weatherDatabase);
 
 // error handler
 app.use(function(err, req, res, next) {
